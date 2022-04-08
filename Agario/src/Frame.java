e731a5f8ca5084d77b55dc3d52c35d7cb04f7422
1860094 <1860094@01-19850.pusd.dom>
2022-04-08 12:19:03 -0700

    Initial Commit

---
+++ Agario/src/Frame.java
@@ -0,0 +1,146 @@
+
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+
+public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
+	
+
+	static int p1Score = 0;
+
+
+	
+	
+	BGMain mainM = new BGMain(0,0);
+	Background 	b = new Background(0, 0);
+	Hat hat = new Hat(0,0);
+	//create music
+	
+	
+	
+	
+	
+	
+	
+	
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		//b.paint(g);
+		mainM.paint(g);
+		hat.paint(g);
+		
+	}
+	
+	
+	
+	
+	public static void score () {
+		
+		p1Score ++;
+		
+	}
+	
+	public static void startDisplay () {
+		
+		
+		
+	}
+	
+	
+public static void main(String[] arg) {
+		Frame f = new Frame();
+	}
+	
+	
+	public Frame() {
+		JFrame f = new JFrame("Flappy Bird");
+		f.setSize(new Dimension(1050, 1000));
+		f.setBackground(Color.blue);
+		f.add(this);
+		f.setResizable(false);
+		f.setLayout(new GridLayout(1,2));
+		f.addMouseListener(this);
+		f.addKeyListener(this);
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override   
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println(arg0.getButton());
+	
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println(arg0.getKeyCode());
+		if(arg0.getKeyCode()==37) {
+			hat.moveL();
+			
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Agario/src/Hat.java
@@ -0,0 +1,91 @@
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Hat{
+	//add location attributes
+	public int x, y;//position of the pillar
+	private int vx; // velocty of x
+	
+	int w = 60;
+	int ph = 160;
+	
+	
+	
+	
+	private Image img; 	
+	private AffineTransform tx;
+
+	public Hat(int x, int y) {
+		this.x = x;
+		this.y = y;
+		img = getImage("/imgs/hatB.png"); //load the image for Tree
+
+		tx = AffineTransform.getTranslateInstance(x, y );
+		init(x, y); 				//initialize the location of the image
+									//use your variables
+	}
+	
+	
+	
+	public void changePicture(String newFileName) {
+		img = getImage(newFileName);
+		init(x, y);
+	}
+	
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+
+		//call update to update the actualy picture location
+		update();
+		g2.drawImage(img, tx, null);
+		
+		
+	}
+	
+	public void moveL() {
+		 vx -=5;
+		
+		//find a cap for vy
+	}
+	
+	/* update the picture variable location */
+	private void update() {
+		
+		
+		
+		x += vx;
+		
+		
+		tx.setToTranslation(x, y);
+		tx.scale(1	, 1);
+		
+		//cap for vy
+		
+		
+	}
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(.5, .5);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Hat.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+
+}
